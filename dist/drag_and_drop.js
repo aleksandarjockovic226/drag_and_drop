c025424dfb039025a3cd73e3255afea336198b04
Aleksandar Jocković <aleksandarjockovic226@gmail.com>
2024-04-25 18:42:39 +0200

    drag and clone

--- dist/drag_and_drop.js
+++ dist/drag_and_drop.js
@@ -1,61 +1,85 @@
 "use strict";
 (function () {
-    const dragAndDropHolders = document.querySelectorAll('[drag_and_drop=true]');
-    if (!dragAndDropHolders) {
+    const dragAndSortHolders = document.querySelectorAll('[drag_and_sort=true]');
+    if (!dragAndSortHolders) {
         return;
     }
-    dragAndDropHolders.forEach(holder => {
-        holder.addEventListener('dragstart', function (event) {
-            const target = event.target;
-            if (!target) {
-                return;
-            }
-            if (target.getAttribute('draggable') !== 'true') {
-                return;
-            }
-            target.classList.add('dragging');
+    dragAndSortHolders.forEach((holder) => {
+        holder.addEventListener('dragstart', (event) => {
+            dragstartHandle(event, holder);
         });
-        holder.addEventListener('dragend', function (event) {
-            const target = event.target;
-            if (!target) {
-                return;
-            }
-            if (target.getAttribute('draggable') !== 'true') {
-                return;
-            }
-            target.classList.remove('dragging');
+        holder.addEventListener('dragend', (event) => {
+            dragendHandle(event, holder);
         });
-        holder.addEventListener('dragover', function (event) {
-            event.preventDefault();
-            const target = event.target;
-            if (!target) {
-                return;
-            }
-            if (target.getAttribute('container') !== 'true') {
-                return;
-            }
-            const afterElement = getDragAfterElement(target, event.clientY);
-            const draggable = holder.querySelector('.dragging');
-            if (afterElement == null) {
-                target.appendChild(draggable);
-            }
-            else {
-                target.insertBefore(draggable, afterElement);
-            }
+        holder.addEventListener('dragover', (event) => {
+            dragoverHandle(event, holder);
         });
     });
-    function getDragAfterElement(container, y) {
-        var _a;
-        const draggableElements = Array.from(container.querySelectorAll('[draggable=true]:not(.dragging)'));
-        return (_a = draggableElements.reduce((closest, child) => {
-            const box = child.getBoundingClientRect();
-            const offset = y - box.top - box.height / 2;
-            if (offset < 0 && offset > (closest ? closest.offset : Number.NEGATIVE_INFINITY)) {
-                return { offset: offset, element: child };
-            }
-            else {
-                return closest;
-            }
-        }, null)) === null || _a === void 0 ? void 0 : _a.element;
-    }
 })();
+(function () {
+    const dragAndCloneHolders = document.querySelectorAll('[drag_and_clone=true]');
+    if (!dragAndCloneHolders) {
+        return;
+    }
+    dragAndCloneHolders.forEach((holder) => {
+        let draggableClone;
+        holder.addEventListener('dragstart', (event) => {
+            dragstartHandle(event, holder);
+            const target = event.target;
+            draggableClone = target.cloneNode(true);
+            draggableClone.classList.add('dragging');
+            draggableClone.style.display = "none";
+        });
+        holder.addEventListener('dragend', (event) => {
+            dragendHandle(event, holder);
+        });
+        holder.addEventListener('dragover', (event) => {
+            dragoverHandle(event, holder, draggableClone);
+            draggableClone.classList.remove('dragging');
+            draggableClone.removeAttribute("style");
+        });
+    });
+})();
+function getDragAfterElement(container, y) {
+    var _a;
+    const draggableElements = Array.from(container.querySelectorAll('[draggable=true]:not(.dragging)'));
+    return (_a = draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect();
+        const offset = y - box.top - box.height / 2;
+        if (offset < 0 && offset > (closest ? closest.offset : Number.NEGATIVE_INFINITY)) {
+            return { offset: offset, element: child };
+        }
+        else {
+            return closest;
+        }
+    }, null)) === null || _a === void 0 ? void 0 : _a.element;
+}
+function dragstartHandle(event, holder) {
+    const target = event.target;
+    if (!target || target.getAttribute('draggable') !== 'true') {
+        return;
+    }
+    target.classList.add('dragging');
+}
+function dragendHandle(event, holder) {
+    const target = event.target;
+    if (!target || target.getAttribute('draggable') !== 'true') {
+        return;
+    }
+    target.classList.remove('dragging');
+}
+function dragoverHandle(event, holder, cloneElement) {
+    event.preventDefault();
+    const target = event.target;
+    if (!target || target.getAttribute('container') !== 'true') {
+        return;
+    }
+    const afterElement = getDragAfterElement(target, event.clientY);
+    const draggable = cloneElement ? cloneElement : holder.querySelector('.dragging');
+    if (afterElement == null) {
+        target.appendChild(draggable);
+    }
+    else {
+        target.insertBefore(draggable, afterElement);
+    }
+}
--- src/drag_and_drop.ts
+++ src/drag_and_drop.ts
@@ -1,77 +1,100 @@
 (function () {
-    const dragAndDropHolders: NodeListOf<HTMLElement> = document.querySelectorAll('[drag_and_drop=true]')
-
-    if (!dragAndDropHolders) {
+    const dragAndSortHolders: NodeListOf<HTMLElement> = document.querySelectorAll('[drag_and_sort=true]')
+    if (!dragAndSortHolders) {
         return;
     }
 
-    dragAndDropHolders.forEach(holder => {
-
-        holder.addEventListener('dragstart', function (event) {
-            const target = event.target as HTMLElement;
-
-            if (!target) {
-                return;
-            }
-
-            if (target.getAttribute('draggable') !== 'true') {
-                return;
-            }
-
-            target.classList.add('dragging');
+    dragAndSortHolders.forEach((holder) => {
+        holder.addEventListener('dragstart', (event) => {
+            dragstartHandle(event, holder);
         });
-
-        holder.addEventListener('dragend', function (event) {
-            const target = event.target as HTMLElement;
-
-            if (!target) {
-                return;
-            }
-
-            if (target.getAttribute('draggable') !== 'true') {
-                return;
-            }
-
-            target.classList.remove('dragging');
+        holder.addEventListener('dragend', (event) => {
+            dragendHandle(event, holder);
         });
-
-        holder.addEventListener('dragover', function (event) {
-            event.preventDefault();
-            const target = event.target as HTMLElement;
-
-            if (!target) {
-                return;
-            }
-
-            if (target.getAttribute('container') !== 'true') {
-                return;
-            }
-
-            const afterElement = getDragAfterElement(target, event.clientY);
-            const draggable = holder.querySelector('.dragging')!;
-
-            if (afterElement == null) {
-                target.appendChild(draggable);
-            } else {
-                target.insertBefore(draggable, afterElement);
-            }
-
-        })
+        holder.addEventListener('dragover', (event) => {
+            dragoverHandle(event, holder);
+        });
     });
 
-    function getDragAfterElement(container: HTMLElement, y: number) {
-        const draggableElements = Array.from(container.querySelectorAll<HTMLElement>('[draggable=true]:not(.dragging)'));
+})();
 
-        return draggableElements.reduce((closest: { offset: number, element: HTMLElement } | null, child: HTMLElement) => {
-            const box = child.getBoundingClientRect();
-            const offset = y - box.top - box.height / 2;
-
-            if (offset < 0 && offset > (closest ? closest.offset : Number.NEGATIVE_INFINITY)) {
-                return { offset: offset, element: child };
-            } else {
-                return closest;
-            }
-        }, null)?.element;
+(function () {
+    const dragAndCloneHolders: NodeListOf<HTMLElement> = document.querySelectorAll('[drag_and_clone=true]')
+    if (!dragAndCloneHolders) {
+        return;
     }
 
-})()
+    dragAndCloneHolders.forEach((holder) => {
+        let draggableClone: HTMLElement;
+
+        holder.addEventListener('dragstart', (event) => {
+            dragstartHandle(event, holder);
+
+            const target = event.target as HTMLElement;
+            draggableClone = target.cloneNode(true) as HTMLElement;
+            draggableClone.classList.add('dragging');
+            draggableClone.style.display = "none";
+        });
+        holder.addEventListener('dragend', (event) => {
+            dragendHandle(event, holder);
+        });
+        holder.addEventListener('dragover', (event) => {
+            dragoverHandle(event, holder, draggableClone);
+
+            draggableClone.classList.remove('dragging');
+            draggableClone.removeAttribute("style");
+        });
+    });
+
+})();
+
+function getDragAfterElement(container: HTMLElement, y: number) {
+    const draggableElements = Array.from(container.querySelectorAll<HTMLElement>('[draggable=true]:not(.dragging)'));
+
+    return draggableElements.reduce((closest: { offset: number, element: HTMLElement } | null, child: HTMLElement) => {
+        const box = child.getBoundingClientRect();
+        const offset = y - box.top - box.height / 2;
+
+        if (offset < 0 && offset > (closest ? closest.offset : Number.NEGATIVE_INFINITY)) {
+            return { offset: offset, element: child };
+        } else {
+            return closest;
+        }
+    }, null)?.element;
+}
+
+function dragstartHandle(event: DragEvent, holder: HTMLElement) {
+    const target = event.target as HTMLElement;
+    if (!target || target.getAttribute('draggable') !== 'true') {
+        return;
+    }
+
+    target.classList.add('dragging');
+}
+
+function dragendHandle(event: DragEvent, holder: HTMLElement) {
+    const target = event.target as HTMLElement;
+    if (!target || target.getAttribute('draggable') !== 'true') {
+        return;
+    }
+
+    target.classList.remove('dragging');
+}
+
+function dragoverHandle(event: DragEvent, holder: HTMLElement, cloneElement?: HTMLElement) {
+    event.preventDefault();
+    const target = event.target as HTMLElement;
+    if (!target || target.getAttribute('container') !== 'true') {
+        return;
+    }
+
+    const afterElement = getDragAfterElement(target, event.clientY);
+
+    const draggable = cloneElement ? cloneElement : holder.querySelector('.dragging')!;
+
+    if (afterElement == null) {
+        target.appendChild(draggable!);
+    } else {
+        target.insertBefore(draggable!, afterElement);
+    }
+}
